53073ee0f32dad38815ff69aeb2ccdb2adcfe892
Ala Ropat <ala_ropat@epam.com >
2014-08-19 17:59:07 +0300

    changed

--- Airport1/src/com/epam/lesson2/entity/Airline.java
+++ Airport1/src/com/epam/lesson2/entity/Airline.java
@@ -3,14 +3,14 @@ package com.epam.lesson2.entity;
 import java.util.ArrayList;
 
 public class Airline {
-private ArrayList<Plane>planes;
+	private ArrayList<Plane> planes;
 
-public ArrayList<Plane> getPlanes() {
-	return planes;
-}
+	public ArrayList<Plane> getPlanes() {
+		return planes;
+	}
 
-public void setPlanes(ArrayList<Plane> planes) {
-	this.planes = planes;
-}
+	public void setPlanes(ArrayList<Plane> planes) {
+		this.planes = planes;
+	}
 
 }
