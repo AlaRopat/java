d6242776fe4977d36c3ab7ad80942d108f634229
Ala Ropat <ala_ropat@epam.com >
2014-08-19 17:58:04 +0300

    added setters and getters

--- Airport1/src/com/epam/lesson2/entity/Airline.java
+++ Airport1/src/com/epam/lesson2/entity/Airline.java
@@ -1,5 +1,16 @@
 package com.epam.lesson2.entity;
 
+import java.util.ArrayList;
+
 public class Airline {
+private ArrayList<Plane>planes;
+
+public ArrayList<Plane> getPlanes() {
+	return planes;
+}
+
+public void setPlanes(ArrayList<Plane> planes) {
+	this.planes = planes;
+}
 
 }
