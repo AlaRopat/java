e408bd3fba4535981dab40a5eb5579e3e4839df9
Ala <ropatalla@gmail.com>
2014-08-31 00:32:16 +0300

    new classes

---
+++ Task#3/src/com/epam/lesson3/Text.java
@@ -0,0 +1,8 @@
+package com.epam.lesson3;
+
+import java.util.ArrayList;
+
+public class Text {
+	private ArrayList<TextElement> list = new ArrayList<TextElement>();
+	
+}
---
+++ Task#3/src/com/epam/lesson3/TextItem.java
@@ -0,0 +1,17 @@
+package com.epam.lesson3;
+
+public class TextItem {
+	private String content = new String();
+
+	public TextItem(String content) {
+		this.content = content;
+	}
+
+	public String getTextItem() {
+		return this.content;
+	}
+
+	public void setTextItem(String content) {
+		this.content = content;
+	}
+}
