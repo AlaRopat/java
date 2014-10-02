5ca33f5863b96525afffb4cec58d77fc72cc7d3a
Ala Ropat <ala_ropat@epam.com>
2014-10-02 20:07:58 +0300

    webdriver

---
+++ Task1/src/main/java/com/epam/Ala_Ropat/WebDriver/App.java
@@ -0,0 +1,13 @@
+package com.epam.Ala_Ropat.WebDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
