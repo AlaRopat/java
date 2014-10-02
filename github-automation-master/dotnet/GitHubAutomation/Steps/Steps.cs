293b97903d4fb64ceebeff0621a4670c5c42121a
Ala Ropat <ala_ropat@epam.com>
2014-10-02 19:26:51 +0300

    new

---
+++ github-automation-master/dotnet/GitHubAutomation/Steps/Steps.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitHubAutomation.Configuration;
+using log4net;
+
+namespace GitHubAutomation.Steps
+{
+    public class Steps
+    {
+        IWebDriver driver;
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Steps));
+
+        public void InitBrowser()
+        {
+            driver = Driver.DriverInstance.GetInstance();
+        }
+
+        public void CloseBrowser()
+        {
+            Driver.DriverInstance.CloseBrowser();
+        }
+
+        public void LoginGithub(string username, string password)
+        {
+            logger.Info("Log in to github");
+            Pages.LoginPage loginPage = new Pages.LoginPage(driver);
+            loginPage.OpenPage();
+            loginPage.Login(username, password);
+        }
+
+        public bool IsLoggedIn(string username)
+        {
+            Pages.LoginPage loginPage = new Pages.LoginPage(driver);
+            return (loginPage.GetLoggedInUserName().Trim().ToLower().Equals(username));
+        }
+
+        public bool CreateNewRepository(string repositoryName, string repositoryDescription)
+        {
+            logger.Info("Creating new repo with name: " + repositoryName);
+            Pages.MainPage mainPage = new Pages.MainPage(driver);
+            mainPage.ClickOnCreateNewRepositoryButton();
+            Pages.CreateNewRepositoryPage createNewRepositoryPage = new Pages.CreateNewRepositoryPage(driver);
+            string expectedRepoName = createNewRepositoryPage.CreateNewRepository(repositoryName, repositoryDescription);
+
+            return expectedRepoName.Equals(createNewRepositoryPage.GetCurrentRepositoryName());
+        }
+
+        public bool CurrentRepositoryIsEmpty()
+        {
+            Pages.CreateNewRepositoryPage createNewRepositoryPage = new Pages.CreateNewRepositoryPage(driver);
+            return createNewRepositoryPage.IsCurrentRepositoryEmpty();
+        }
+    }
+}
